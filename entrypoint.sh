c2e5916f527c6ceb437e11f5c8a30484ae980de3
brx86 <1124852137@qq.com>
2021-06-21 02:06:03 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ git clone "https://aur.archlinux.org/$1.git"
 cd "$1"
 sed -i 's|//packages.deepin.com|//aya1.xyz/https://packages.deepin.com|g' PKGBUILD
 sed -i 's|//community-store-packages.deepin.com|//aya1.xyz/https://community-store-packages.deepin.com|g' PKGBUILD
-pacman -S --needed $(grep  "makedepends = " .SRCINFO|sed 's|makedepends = ||g')
+pacman -S --needed --noconfirm $(grep  "makedepends = " .SRCINFO|sed 's|makedepends = ||g')
 makepkg -sf --noconfirm --skippgpcheck --nodeps
