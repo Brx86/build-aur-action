ea031fc15d1e1ef11414ef6553c14ed5d6bf5668
brx86 <1124852137@qq.com>
2021-06-21 07:52:59 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-pacman -U --noconfirm https://github.com/Brx86/build-aur-action/releases/download/v1.0/yay-10.2.3-1-x86_64.pkg.tar.zst
+wget https://github.com/Brx86/build-aur-action/releases/download/v1.0/yay-10.2.3-1-x86_64.pkg.tar.zst
+pacman -U --noconfirm ./*.pkg.tar.zst
 mkdir $1
 yay -S $1 --noconfirm
 mv ~/.cache/yay/*/*.pkg.tar.zst ./$1/
