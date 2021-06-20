ce2ef6890611ba73a1b3d446cb5cbdccfa90cf6c
Brx86 <44391900+Brx86@users.noreply.github.com>
2021-06-20 11:21:14 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-set -euo pipefail
-
-
-
-pacman -Syu --noconfirm --needed python p7zip bash-completion npm yarn asar
 git clone "https://aur.archlinux.org/$1.git"
 cd "$1"
 sed 's|packages.deepin.com|aya1.xyz/https://packages.deepin.com|g' PKGBUILD
