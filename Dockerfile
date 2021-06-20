61633302c0d34a72031ffffa41a50770b740ff8b
brx86 <1124852137@qq.com>
2021-06-21 01:14:44 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM archlinux:latest
 RUN pacman -Syu base-devel git python --needed --noconfirm --overwrite '*' && sed -i '/E_ROOT/d' /usr/bin/makepkg
-RUN pacman -U https://github.com/Brx86/build-aur-action/releases/download/v1.0/yay-10.2.3-1-x86_64.pkg.tar.zst
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-git clone "https://aur.archlinux.org/$1.git"
+yay -Ga $1
 cd "$1"
 sed -i 's|//packages.deepin.com|//aya1.xyz/https://packages.deepin.com|g' PKGBUILD
 sed -i 's|//community-store-packages.deepin.com|//aya1.xyz/https://community-store-packages.deepin.com|g' PKGBUILD
+pacman -S --needed $(grep  "makedepends = " .SRCINFO|sed 's|makedepends = ||g')
 makepkg -sf --noconfirm --skippgpcheck --nodeps
