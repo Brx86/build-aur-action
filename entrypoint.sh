83a1445d20b08f88e3423fa1269832c6d9e7f1eb
Brx86 <44391900+Brx86@users.noreply.github.com>
2021-06-20 12:48:08 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git clone "https://aur.archlinux.org/$1.git"
 cd "$1"
+sed -i 's|packages.deepin.com|aya1.xyz/https://packages.deepin.com|g' PKGBUILD
 sed -i 's|community-store-packages.deepin.com|aya1.xyz/https://community-store-packages.deepin.com|g' PKGBUILD
 makepkg -sf --noconfirm --skippgpcheck --nodeps
