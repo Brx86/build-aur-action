20ac44724e71b91e23a217f08fd3582528ab6205
Brx86 <44391900+Brx86@users.noreply.github.com>
2021-06-20 10:28:54 +0800

    Update entrypoint.sh
    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+set -euo pipefail
 
+cat << AYA >> /etc/pacman.conf
+[multilib]
+Include = /etc/pacman.d/mirrorlist
+
+[archlinuxcn]
+SigLevel = Never
+Server = https://mirrors.bfsu.edu.cn/archlinuxcn/$arch
+AYA
+
+pacman -Syu --noconfirm --needed python p7zip bash-completion npm yarn asar
 git clone "https://aur.archlinux.org/$1.git"
 cd "$1"
+sed 's|packages.deepin.com|aya1.xyz/https://packages.deepin.com|g' PKGBUILD
 makepkg -sf --noconfirm --skippgpcheck --nodeps
