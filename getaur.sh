1d4984fc95df603f136bf773f1224446e2889765
brx86 <1124852137@qq.com>
2021-06-21 17:24:15 +0800

    update

--- getaur.sh
+++ getaur.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 curl https://aur.archlinux.org/packages.gz -s|gzip -d - >packages
 sed -i '1d' packages
+sum=$(cat packages|wc -l )
+num=1
 pkgs=$(cat packages)
 mkdir aur
 for i in ${pkgs[*]}; do 
+    echo $num/$sum
     git clone https://aur.archlinux.org/$i aur/$i
+    num=$(($num+1))
 done
 tar cvf aur.tar.xz aur
