a3960689407af85f6928a01d98aefaa35eeb6a0f
brx86 <1124852137@qq.com>
2021-06-21 08:37:55 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget https://mirrors.bfsu.edu.cn/archlinuxcn/x86_64/pikaur-1.6.17-1-any.pkg.tar.zst
-pacman -U ./*.pkg.tar.zst
+pacman -U ./*.pkg.tar.zst --noconfirm
 mkdir $1
 pikaur -S $1 --noconfirm
 mv ~/.cache/pikaur/pkg/*.pkg.tar.zst ./$1/
