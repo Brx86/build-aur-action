3afb1f5d4659259fe0b9ff4e6e824d39e74e97b8
brx86 <1124852137@qq.com>
2021-06-21 09:35:22 +0800

    update

--- entrypoint2.sh
+++ entrypoint2.sh
@@ -4,14 +4,13 @@ set -euo pipefail
 cat << EOM >> /etc/pacman.conf
 [multilib]
 Include = /etc/pacman.d/mirrorlist
-[archlinuxcn]
-SigLevel = Never
-Server = https://mirrors.bfsu.edu.cn/archlinuxcn/$arch
 EOM
 
-pacman -Syu --noconfirm --needed yay mlocate
+pacman -Sy
 
-#pacman --noconfirm -U https://github.com/Brx86/build-aur-action/releases/download/v1.0/yay-10.2.3-1-x86_64.pkg.tar.zst
+#pacman -Syu --noconfirm --needed yay mlocate
+
+pacman --noconfirm -U https://mirrors.bfsu.edu.cn/archlinuxcn/x86_64/yay-10.2.2-4-x86_64.pkg.tar.zst
 
 useradd builder -m
 
