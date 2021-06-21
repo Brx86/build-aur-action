b2034fea837a9ad4110b9bf712996f22102e7dff
brx86 <1124852137@qq.com>
2021-06-21 09:37:39 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM archlinux:latest
-RUN pacman -Syu base-devel git mlocate --needed --noconfirm --overwrite '*' && sed -i '/E_ROOT/d' /usr/bin/makepkg
+RUN pacman -Syu base-devel git --needed --noconfirm --overwrite '*' && sed -i '/E_ROOT/d' /usr/bin/makepkg
 RUN patched_glibc=glibc-linux4-2.33-5-x86_64.pkg.tar.zst && \
     curl -LO "https://repo.archlinuxcn.org/x86_64/$patched_glibc" && \
     bsdtar -C / -xvf "$patched_glibc"
--- entrypoint2.sh
+++ entrypoint2.sh
@@ -4,13 +4,14 @@ set -euo pipefail
 cat << EOM >> /etc/pacman.conf
 [multilib]
 Include = /etc/pacman.d/mirrorlist
+[archlinuxcn]
+SigLevel = Never
+Server = https://mirrors.bfsu.edu.cn/archlinuxcn/x86_64
 EOM
 
-pacman -Sy
+pacman -Syu --noconfirm --needed yay mlocate
 
-#pacman -Syu --noconfirm --needed yay mlocate
-
-pacman --noconfirm -U https://mirrors.bfsu.edu.cn/archlinuxcn/x86_64/yay-10.2.2-4-x86_64.pkg.tar.zst
+#pacman --noconfirm -U https://github.com/Brx86/build-aur-action/releases/download/v1.0/yay-10.2.3-1-x86_64.pkg.tar.zst
 
 useradd builder -m
 
