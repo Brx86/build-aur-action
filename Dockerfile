767afc515131610642654bbde379ac75d06c3550
brx86 <1124852137@qq.com>
2021-06-21 09:41:32 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,4 @@
 FROM archlinux:latest
 RUN pacman -Syu base-devel git --needed --noconfirm --overwrite '*' && sed -i '/E_ROOT/d' /usr/bin/makepkg
-RUN patched_glibc=glibc-linux4-2.33-5-x86_64.pkg.tar.zst && \
-    curl -LO "https://repo.archlinuxcn.org/x86_64/$patched_glibc" && \
-    bsdtar -C / -xvf "$patched_glibc"
 COPY entrypoint2.sh /entrypoint2.sh
 ENTRYPOINT ["/entrypoint2.sh"]
--- entrypoint2.sh
+++ entrypoint2.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 set -euo pipefail
-
-cat << EOM >> /etc/pacman.conf
+mkdir $1
+cat << AYA >> /etc/pacman.conf
 [multilib]
 Include = /etc/pacman.d/mirrorlist
 [archlinuxcn]
 SigLevel = Never
 Server = https://mirrors.bfsu.edu.cn/archlinuxcn/x86_64
-EOM
-
+AYA
 pacman -Syu --noconfirm --needed yay mlocate
-
-#pacman --noconfirm -U https://github.com/Brx86/build-aur-action/releases/download/v1.0/yay-10.2.3-1-x86_64.pkg.tar.zst
-
 useradd builder -m
-
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-
-sudo -u builder yay -Sa ttf-hymmnos-ar --noconfirm --nodiffmenu --nocleanmenu
-
-updatedb
-
-locate ttf-hymmnos-ar
-
-
+sudo -u builder yay -Sa $1 --noconfirm --nodiffmenu --nocleanmenu
+cp /home/builder/.cache/yay/*/*.pkg.tar.zst ./$1/
