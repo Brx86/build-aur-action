f70037e38c4354fba271b7b927dfd9d383a55ac3
brx86 <1124852137@qq.com>
2021-06-21 08:51:27 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 git clone https://github.com/actionless/pikaur
 
 mkdir $1
-python3 ./pikaur/pikaur.py -S  -S $1 --noconfirm
+python ./pikaur/pikaur.py -S  -S $1 --noconfirm
 mv ~/.cache/pikaur/pkg/*.pkg.tar.zst ./$1/
