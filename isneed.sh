b4bad3a617e69f9e42a3fcea6e92e293d0b4a9ca
brx86 <1124852137@qq.com>
2021-11-27 17:39:44 +0800

    fix

--- isneed.sh
+++ isneed.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 pkgname=$1
 repoVer=$(yay -S --print-format "%v" $pkgname)
-aurVer=$(yay -Sa --print-format "%v" $pkgname)
-if [ "$repoVer" == "$aurVer" ]; then
+aurVer=$(curl -s "https://aur.archlinux.org/rpc/?v=5&type=search&arg=$pkgname"|jq '.results[0]'|jq '.Version')
+if [ "\"$repoVer\"" == "$aurVer" ]; then
     echo "$pkgname is up to date"
 else
     yay -Sa $1 --noconfirm --nodiffmenu --nocleanmenu
